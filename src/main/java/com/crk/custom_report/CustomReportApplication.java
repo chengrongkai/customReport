b7f571ecd052a4c104348a32c2768cee5ffbdfec
程荣凯 <1058831654@qq.com>
2018-08-27 21:08:25 +0800

    Initial commit

---
+++ src/main/java/com/crk/custom_report/CustomReportApplication.java
@@ -0,0 +1,12 @@
+package com.crk.custom_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomReportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomReportApplication.class, args);
+    }
+}
